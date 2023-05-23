c406370fb1d824f610b59a3045d2086ff199ec35
Tiago Ferrari <69052612+tiagoferrari-projects@users.noreply.github.com>
2023-05-23 15:48:08 -0300

    feat(VueRouter)
    add VueRouter

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PageHome from '@/components/PageHome'
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [{
+  path: '/',
+  component: PageHome
+}]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
 
 const firstProject = createApp(App)
+firstProject.use(router)
 firstProject.mount('#app')
