ac6076638247734165e2d30518dba23b0f0ce7ea
Tiago Ferrari <69052612+tiagoferrari-projects@users.noreply.github.com>
2023-05-26 01:13:59 -0300

    feat(): PageThreadShow
    Routing & Components Basics

---
+++ src/components/PageThreadShow.vue
@@ -0,0 +1,165 @@
+<template>
+  <div class="col-large push-top">
+    <h1>{{ thread.title }}</h1>
+
+    <div class="post-list">
+      <div class="post" v-for="postId in threads.posts" :key="postId">
+        <div class="user-info">
+          <a href="#" class="user-name">{{
+            userById(postById(postId).userId).name
+          }}</a>
+
+          <a href="#">
+            <img
+              class="avatar-large"
+              :src="userById(postById(postId).userId).avatar"
+              alt=""
+            />
+          </a>
+
+          <p class="desktop-only text-small">107 posts</p>
+        </div>
+
+        <div class="post-content">
+          <div>
+            <p>
+              {{ postsById(postId).text }}
+            </p>
+          </div>
+        </div>
+
+        <div class="post-date text-faded">
+          {{ postsById(postId).publishedAt }}
+        </div>
+      </div>
+
+      <div class="post">
+        <div class="user-info">
+          <a href="profile.html#profile-details" class="user-name"
+            >Joseph Kerr</a
+          >
+
+          <a href="profile.html#profile-details">
+            <img
+              class="avatar-large"
+              src="https://i.imgur.com/OqlZN48.jpg"
+              alt=""
+            />
+          </a>
+
+          <p class="desktop-only text-small">116 posts</p>
+
+          <p class="desktop-only text-small">73 threads</p>
+
+          <span class="online desktop-only">online</span>
+        </div>
+
+        <div class="post-content">
+          <div>
+            <blockquote class="small">
+              <div class="author">
+                <a href="/user/robin" class=""> robin</a>
+                <span class="time">a month ago</span>
+                <i class="fa fa-caret-down"></i>
+              </div>
+
+              <div class="quote">
+                <p>
+                  Is horseradish and Wasabi the same thing? I've heard so many
+                  different things.
+                </p>
+              </div>
+            </blockquote>
+            <p>They're not the same!</p>
+          </div>
+          <a class="edit-post link-unstyled"><i class="fa fa-pencil"></i></a>
+        </div>
+
+        <div class="post-date text-faded">6 hours ago</div>
+
+        <div class="reactions">
+          <button class="btn-xsmall">
+            + <i class="fa fa-smile-o emoji"></i>
+          </button>
+        </div>
+      </div>
+
+      <div class="post">
+        <div class="user-info">
+          <a href="profile.html#profile-details" class="user-name"
+            >Ray-Nathan James</a
+          >
+
+          <a href="profile.html#profile-details">
+            <img
+              class="avatar-large"
+              src="https://firebasestorage.googleapis.com/v0/b/forum-2a982.appspot.com/o/images%2Favatars%2Fraynathan?alt=media&amp;token=bd9a0f0e-60f2-4e60-b092-77d1ded50a7e"
+              alt=""
+            />
+          </a>
+
+          <p class="desktop-only text-small">10 posts</p>
+
+          <p class="desktop-only text-small">4 threads</p>
+
+          <span class="offline desktop-only">offline</span>
+        </div>
+
+        <div class="post-content">
+          <div>
+            <p>
+              <a href="/user/Joker" class="">@Joker</a> is right, they're not
+              the same.
+            </p>
+            <p>
+              They are different plants from the same family (mustard/cabbage).
+            </p>
+          </div>
+        </div>
+
+        <div class="post-date text-faded">6 hours ago</div>
+
+        <div class="reactions">
+          <button class="btn-xsmall">
+            + <i class="fa fa-smile-o emoji"></i>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import sourceData from '@/assets/data.json'
+export default {
+  props:{
+    id:{
+      required: true,
+      type: String
+    }
+  },
+  data () {
+    return {
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users
+    }
+  },
+  computed:{
+    thread() {
+      return this.threads.find(thread => thread.id === this.id) // also avaliable under this.$route.oarams.id
+    }
+  },
+  methods: {
+    postById (postId) {
+      return this.posts.find((p) => p.id === postId)
+    },
+    userById (userId) {
+      return this.users.find((p) => p.id === userId)
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,17 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import PageHome from '@/components/PageHome'
-import { createRouter, createWebHistory } from 'vue-router'
-
-const routes = [{
-  path: '/',
-  component: PageHome
-}]
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes
-})
+import router from '@/router/index'
 
 const firstProject = createApp(App)
 firstProject.use(router)
