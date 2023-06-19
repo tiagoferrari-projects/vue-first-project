5aefd3238efc7c3b3386bce822e0d2ed66434cae
Tiago Ferrari <69052612+tiagoferrari-projects@users.noreply.github.com>
2023-06-19 05:06:24 -0300

    feat(): stopped in class six, chapter four

---
+++ src/components/PostEditor.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="col-full">
+    <form @submit.prevent="save">
+      <div class="form-group">
+        <textarea v-model="text" :value="text" name="" id="" cols="30" rows="10"
+          class="form-input"></textarea>
+      </div>
+      <div class="form-actions">
+        <button class="btn-blue">Submit posts</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      text: ''
+    }
+  },
+  methods: {
+    save () {
+      const postId = 'gggg' + Math.random()
+      const post = {
+        id: postId,
+        text: this.text,
+        publishedAt: Math.floor(Date.now() / 1000),
+        userId: 'rpbB8C6ifrYmNDufMERWfQUoa202'
+      }
+      this.$emit('save', { post }) // access under eventData.post
+      this.text = ''
+    }
+  }
+}
+</script>
+
+<style scoped></style>
